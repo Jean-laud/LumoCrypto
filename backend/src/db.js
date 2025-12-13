c4d42e682417965a0d2f11aeb35f1b84ab4dad2b
Jean-laud <jeanlaudclermy@gmail.com>
2025-12-13 22:58:40 +0100

    Changement de bdd

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "lumocrypto",
+  database: "lumocrypto15",
   password: "yoshicool37",
   port: 5432,
 });
